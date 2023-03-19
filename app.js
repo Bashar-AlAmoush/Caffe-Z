b607acbfbf972192bc55babd75cd2b01eb8407e2
Bashar <omushb@gmail.com>
2023-03-19 15:52:32 +0300

    update  javascipt file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let p = prompt("Enter Your Name? ")
 let g = prompt("Enter your gender? ")
 g=g.toLowerCase()
+while(g !="male" &&  g !="female"){
+    g = prompt("Please enter a correct gender")
+
+    }
 if(g == "male"){
     alert("Welcome Mr. "+p )
 
@@ -17,3 +21,8 @@ let d = prompt("What drink do you want Hot or Cold ?")
 let named = prompt("Name of your drink ?")
 alert("Your drink is being prepared .")
 console.log("Name : "+p +"  Drink name : "+named)
+let a=[p,g,d,named];
+for(let i=0;i<a.length;i++){
+console.log(a[i])
+
+}
